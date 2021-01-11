f35c0e0272d57ab6d785ce791183a4dd9a7ca7cd
krvzl <keion.calling@gmail.com>
2021-01-11 04:26:40 -0800

    Update keymap.c

--- keyboards/reviung41/keymaps/default/keymap.c
+++ keyboards/reviung41/keymaps/default/keymap.c
@@ -28,31 +28,31 @@ enum layer_names {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_BASE] = LAYOUT_reviung41(
-    KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,      KC_T,               KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_BSPC,
-    KC_LCTL,  KC_A,     KC_S,     KC_D,     KC_F,      KC_G,               KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,
-    KC_LSFT,  KC_Z,     KC_X,     KC_C,     KC_V,      KC_B,               KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  RSFT_T(KC_ENT),
-                                            KC_LALT,   LOWER,    KC_SPC,   RAISE,    KC_RGUI
+    KC_TAB,          KC_Q,     KC_W,     KC_F,     KC_P,      KC_G,               KC_J,     KC_L,     KC_U,     KC_Y,     KC_SCLN,  KC_BSPC,
+    LCTL_T(KC_ESC),  KC_A,     KC_R,     KC_S,     KC_T,      KC_D,               KC_H,     KC_N,     KC_E,     KC_I,     KC_O,     KC_QUOT,
+    KC_LSPO,         KC_Z,     KC_X,     KC_C,     KC_V,      KC_B,               KC_K,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSPC,
+                                                   KC_LALT,   LOWER,    KC_SPC,   RAISE,    KC_ENT
   ),
   
   [_LOWER] = LAYOUT_reviung41(
-    _______,  KC_EXLM,  KC_AT,    KC_HASH,  KC_DLR,    KC_PERC,            KC_CIRC,  KC_AMPR,  KC_ASTR,  KC_LPRN,  KC_RPRN,  KC_DEL,
-    _______,  KC_UNDS,  KC_PLUS,  KC_LCBR,  KC_RCBR,   KC_PIPE,            KC_LEFT,  KC_DOWN,  KC_UP,    KC_RGHT,  KC_GRV,   KC_TILD,
-    _______,  KC_ESC,   KC_LGUI,  KC_LALT,  KC_CAPS,   KC_DQUO,            KC_HOME,  KC_END,   KC_PGUP,  KC_PGDN,  KC_PSCR,  RSFT_T(KC_SPC),
-                                            _______,   _______,  KC_ENT,   _______,  _______
+    KC_0,            KC_1,     KC_2,     KC_3,     KC_3,      KC_PERC,            KC_CIRC,  KC_AMPR,  KC_ASTR,  KC_LPRN,  KC_RPRN,  KC_DEL,
+    _______,         KC_4,     KC_5,     KC_6,     KC_6,      KC_PIPE,            KC_LEFT,  KC_DOWN,  KC_UP,    KC_RGHT,  KC_GRV,   KC_TILD,
+    _______,         KC_7,     KC_8,     KC_9,     KC_9,      KC_DQUO,            KC_HOME,  KC_END,   KC_PGUP,  KC_PGDN,  KC_PSCR,  RSFT_T(KC_SPC),
+                                                   _______,   _______,  _______,  _______,  _______
   ),
   
   [_RAISE] = LAYOUT_reviung41(
-    _______,  KC_1,     KC_2,     KC_3,     KC_4,      KC_5,               KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_DEL,
-    _______,  KC_MINS,  KC_EQL,   KC_LBRC,  KC_RBRC,   KC_BSLS,            KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,
-    _______,  KC_ESC,   KC_RGUI,  KC_RALT,  KC_CAPS,   KC_QUOT,            KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,
-                                            _______,   _______,  KC_BSPC,  _______,  _______
+    _______,         KC_F1,    KC_F2,    KC_F3,    KC_F4,     _______,            KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_DEL,
+    _______,         KC_F5,    KC_F6,    KC_F7,    KC_F8,     _______,            KC_F1,    KC_LEFT,  KC_DOWN,  KC_UP,    KC_RGHT,  KC_F6,
+    _______,         KC_F9,    KC_F10,   KC_F11,   KC_F12,    _______,            KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,
+                                                   _______,   _______,  _______,  _______,  KC_RGUI
   ),
   
   [_ADJUST] = LAYOUT_reviung41(
-    RGB_VAI,   RGB_SAI, RGB_HUI,  RGB_MOD,  XXXXXXX,   RGB_TOG,            XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
-    RGB_VAD,   RGB_SAD, RGB_HUD,  RGB_RMOD, XXXXXXX,   XXXXXXX,            XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
-    XXXXXXX,   XXXXXXX, XXXXXXX,  XXXXXXX,  XXXXXXX,   XXXXXXX,            RESET,    XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
-                                            _______,   _______,  XXXXXXX,  _______,  _______
+    RGB_VAI,          RGB_SAI, RGB_HUI,  RGB_MOD,  XXXXXXX,   RGB_TOG,            XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
+    RGB_VAD,          RGB_SAD, RGB_HUD,  RGB_RMOD, XXXXXXX,   XXXXXXX,            XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
+    XXXXXXX,          XXXXXXX, XXXXXXX,  XXXXXXX,  XXXXXXX,   XXXXXXX,            RESET,    XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
+                                                   _______,   _______,  XXXXXXX,  _______,  _______
   ),
 };
 
