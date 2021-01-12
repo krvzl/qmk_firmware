ca0e790b87999a19aca5994367f555bc85a6d571
krvzl <keion.calling@gmail.com>
2021-01-12 04:25:37 -0800

    Update keymap.c

--- keyboards/reviung41/keymaps/default/keymap.c
+++ keyboards/reviung41/keymaps/default/keymap.c
@@ -35,17 +35,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
   
   [_LOWER] = LAYOUT_reviung41(
-    KC_0,            KC_1,     KC_2,     KC_3,     KC_3,      KC_PERC,            KC_CIRC,  KC_AMPR,  KC_ASTR,  KC_LPRN,  KC_RPRN,  KC_DEL,
-    _______,         KC_4,     KC_5,     KC_6,     KC_6,      KC_PIPE,            KC_LEFT,  KC_DOWN,  KC_UP,    KC_RGHT,  KC_GRV,   KC_TILD,
-    _______,         KC_7,     KC_8,     KC_9,     KC_9,      KC_DQUO,            KC_HOME,  KC_END,   KC_PGUP,  KC_PGDN,  KC_PSCR,  KC_ENT,
+    _______,         KC_0,     KC_1,     KC_2,     KC_3,      KC_PERC,            _______,  _______,  _______,  _______,  _______,  KC_DEL,
+    _______,         KC_4,     KC_4,     KC_5,     KC_6,      KC_PIPE,            _______,  _______,  _______,  _______,  _______,  KC_BSLS,
+    _______,         KC_7,     KC_7,     KC_8,     KC_9,      KC_DQUO,            KC_HOME,  KC_END,   KC_PGUP,  KC_PGDN,  KC_PSCR,  KC_ENT,
                                                    _______,   _______,  _______,  _______,  _______
   ),
   
   [_RAISE] = LAYOUT_reviung41(
-    _______,         KC_F1,    KC_F2,    KC_F3,    KC_F4,     _______,            KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_LEAD,
+    _______,         KC_F1,    KC_F2,    KC_F3,    KC_F4,     _______,            KC_6,     KC_MINS,  KC_EQL,   KC_LBRC,  KC_RBRC,  KC_LEAD,
     _______,         KC_F5,    KC_F6,    KC_F7,    KC_F8,     _______,            KC_F1,    KC_LEFT,  KC_DOWN,  KC_UP,    KC_RGHT,  KC_F6,
     _______,         KC_F9,    KC_F10,   KC_F11,   KC_F12,    _______,            KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,
-                                                   _______,   _______,  _______,  _______,  KC_RGUI
+                                                   _______,   _______,  KC_MINS,  _______,  KC_RGUI
   ),
   
   [_ADJUST] = LAYOUT_reviung41(
